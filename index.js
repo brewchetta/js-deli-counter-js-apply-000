e7783043c28319ea4167d39624722f04af666abf
Chett Tiller <chett.tiller@gmail.com>
2018-09-12 17:31:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ var currentLine = function(line) {
   if (line.length == 0) {
     return "The line is currently empty."
   } else {
-    return "The line is currently: " +
+    var string = "The line is currently ";
     for(var i = 0; i < line.length; i++) {
-      return (i+1)+". " + line[i] + " "
+      string = string + (i+1)+". " + line[i] + " ";
+      return string;
     }
   }
 };
